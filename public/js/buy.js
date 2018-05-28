45cfd687928687354b3624db3340ec18d5403ffd
gsy-PC\gsy <siyi.guo@foriseholdings.com>
2018-05-28 18:43:03 +0800

    添加地区三级联动

--- public/js/buy.js
+++ public/js/buy.js
@@ -40,6 +40,7 @@ $(function(){
 
  	function init () {
    		orderStatus ()
+   		address ()
  	};
  	function orderStatus () {
 	    $.ajax({
@@ -582,5 +583,50 @@ $(function(){
 			mail.run();
  		})
  	}
+ 	/**
+ 	 * [mailAutoComplete description]   三级联动
+ 	 * @return {[type]} [description]
+ 	 */
+ 	function address () {
+ 		var html = "";
+	    $("#input_city").append(html);
+	    $("#input_area").append(html);
+	    $.each(pdata,function(idx,item){
+	        if (parseInt(item.level) == 0) {
+	            html += "<option value="+item.code+" >"+ item.names +"</option> ";
+	        }
+	    });
+	    $("#input_province").append(html);
+	    $("#input_province").change(function(){
+	        if ($(this).val() == "") return;
+	        $("#input_city option").remove();
+	        $("#input_area option").remove();
+	        //var code = $(this).find("option:selected").attr("exid");
+	        var code = $(this).find("option:selected").val();
+	        code = code.substring(0,2);
+	        var html = "<option value=''>--请选择--</option>";
+	        $("#input_area option").append(html);
+	        $.each(pdata,function(idx,item){
+	            if (parseInt(item.level) == 1 && code == item.code.substring(0,2)) {
+	                html +="<option value="+item.code+" >"+ item.names +"</option> ";
+	            }
+	        });
+	        $("#input_city ").append(html);
+	    });
+	    $("#input_city").change(function(){
+	        if ($(this).val() == "") return;
+	        $("#input_area option").remove();
+	        var code = $(this).find("option:selected").val();
+	        code = code.substring(0,4);
+	        var html = "<option value=''>--请选择--</option>";
+	        $.each(pdata,function(idx,item){
+	            if (parseInt(item.level) == 2 && code == item.code.substring(0,4)) {
+	                html +="<option value="+item.code+" >"+ item.names +"</option> ";
+	            }
+	        });
+	        $("#input_area ").append(html);
+	    });
+ 	}
+	 	
 })
 
